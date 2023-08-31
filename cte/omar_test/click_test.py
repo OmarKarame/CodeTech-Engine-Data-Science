e2c6a07dd4a30ad7eaf312bcb13235502ebf3b07
Ethan Kho ethanrkho@gmail.com <ethanrkho@gmail.com>
2023-08-31 16:48:37 +0100

    added files changed 31 aug

--- cte/omar_test/click_test.py
+++ cte/omar_test/click_test.py
@@ -12,24 +12,22 @@ def hello(name):
 @click.command()
 @click.option('-r','--repository', prompt='Connect to repository',
               help='The repository you would like to connect to on your github')
-def connect(repo_name):
-    """Simple program that git adds file"""
-    click.echo(f"This is the repo that you are connecting to: {repo_name}!")
 
+def connect(repository):
+    """Simple program that git adds file"""
+    click.echo(f"This is the repo that you are connecting to: {repository}!")
+
+
+
+
+# @click.command()
+# # @click.option('--count', default=1, help='Number of greetings.')
+# @click.option('-af','--add_file', prompt='Add the file name that you want to add to your github',
+#               help='The file on your local host that you want to commit to github')
+# def add_file(file):
+#     """Simple program that git adds file"""
+#     click.echo(f"This is the file that you are adding {file}!")
+#     !git add file
 
 if __name__ == '__main__':
-    add_file()
-
-
-
-@click.command()
-# @click.option('--count', default=1, help='Number of greetings.')
-@click.option('-af','--add_file', prompt='Add the file name that you want to add to your github',
-              help='The file on your local host that you want to commit to github')
-def add_file(file):
-    """Simple program that git adds file"""
-    click.echo(f"This is the file that you are adding {file}!")
-    !git add file
-
-if __name__ == '__main__':
-    add_file()
+    connect()
