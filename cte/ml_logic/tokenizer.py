9ed26b4bc3c8c18b7ab36602c2a1122649d9cb36
Tomasz Cytrycki <tomasz.cytrycki1@gmail.com>
2023-09-01 18:38:23 +0100

    updated transformers and tokenizer, new file created but is not working

--- cte/ml_logic/tokenizer.py
+++ cte/ml_logic/tokenizer.py
@@ -1,11 +1,14 @@
 from transformers import AutoTokenizer
-from transformer import MODEL_CHECKPOINT
 
-new_tokens_list = ["{", "}", "{ }"]
+new_tokens_list = ["{", "}", "{ }", "[sad]", "[ead]", "[ssb]", "[esb]", "[scn]", "[ecn]", r"\n", r"\t", "as", "assert", "def", "for", "continue", "def", "del", "elif", "else", "False", "finally", "import", "lambda", "None", "nonlocal", "raise", "return", "True", "while", "yield"]
 
-def updated_tokenizer(list_of_new_tokens):
+# words already included in tokenizer: and, break, class, except, for, from, if, in, is, nor, or, pass, try, with
+def updated_tokenizer(list_of_new_tokens, model_type):
     """Instantiate new tokenizer updated with data fed"""
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
-    tokenizer.add_tokens(list_of_new_tokens)
+    tokenizer = AutoTokenizer.from_pretrained(model_type)
+    list_of_new_tokens = set(new_tokens_list) - set(tokenizer.vocab.keys())
+    tokenizer.add_tokens(list(list_of_new_tokens))
 
     return tokenizer
+
+
